0ec7adb635bec4bce5fdceda0d3ddf09c8995737
prof <spasmodic51@gmail.com>
2023-09-06 09:25:32 +0200

    Probs modale grand écran réglés

--- src/Pages/Projets/index.jsx
+++ src/Pages/Projets/index.jsx
@@ -11,26 +11,28 @@ const Projets = () => {
 
     const isSticky = useSelector(Sticky)
     
-    return (<motion.div layout transition={{
-        layout: { duration: 0 }
-      }}  variants={routeVariants} initial="initial" animate="final" exit="exit">
+    return (<div>
         <HelmetProvider>
                 <title>Mes projets</title>
             </HelmetProvider>
 
-{isSticky?  <div className="decal2"> <h1>Mes projets</h1>
+{isSticky?  <div><motion.div className="decal2" layout transition={{
+                layout: { duration: 0 }
+              }}  variants={routeVariants} initial="initial" animate="final" exit="exit"> <h1>Mes projets</h1>
             <div className="separator"></div>
             <div className="toBecomeFlex">
             {Datas.map(({id, nom, photo, adresse, GithubCode, description, compétences}) => 
-            <CardProjet id={id} nom={nom} photo={photo} adresse={adresse} git={GithubCode} descr={description} compt={compétences} key={id} />)}</div><ModaleProjet /></div> :  <div> <h1>Mes projets</h1>
+            <CardProjet id={id} nom={nom} photo={photo} adresse={adresse} git={GithubCode} descr={description} compt={compétences} key={id} />)}</div></motion.div><ModaleProjet /></div> :  <div><motion.div layout transition={{
+                layout: { duration: 0 }
+              }}  variants={routeVariants} initial="initial" animate="final" exit="exit"> <h1>Mes projets</h1>
             <div className="separator"></div>
             <div className="toBecomeFlex">
             {Datas.map(({id, nom, photo, adresse, GithubCode, description, compétences}) => 
-            <CardProjet id={id} nom={nom} photo={photo} adresse={adresse} git={GithubCode} descr={description} compt={compétences} key={id} />)}</div><ModaleProjet />
-</div> }
+            <CardProjet id={id} nom={nom} photo={photo} adresse={adresse} git={GithubCode} descr={description} compt={compétences} key={id} />)}</div>
+</motion.div><ModaleProjet /></div> }
           
 
-    </motion.div>)
+    </div>)
 }
 
 export default Projets;
