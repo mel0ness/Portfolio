d0c5ef8d84268bb5f362deda2c3b3a1f524a796c
prof <spasmodic51@gmail.com>
2024-01-25 10:41:03 +0100

    Fin du theme pour la page error

--- src/Pages/Error/index.jsx
+++ src/Pages/Error/index.jsx
@@ -1,12 +1,17 @@
 import Warning from "../../assets/error.svg"
+import DarkWarning from "../../assets/warning-dark.svg"
 import "../../Style/Pages/Error/error.scss"
 import { motion } from "framer-motion"
 import { routeVariants } from "../../Features/routeVariants"
+import {currentTheme} from "../../Features/selector"
+import { useSelector } from "react-redux"
 
 const Error = () => {
+
+    const colors = useSelector(currentTheme)
     return (
-        <motion.div variants={routeVariants} initial="initial" animate="final" exit="exit" key="Error">
-            <img src={Warning} alt="Error" className="warning" />
+        <motion.div variants={routeVariants} initial="initial" animate="final" exit="exit" key="Error" className={"error "+colors}>
+            {colors === "Dark"? <img src={DarkWarning} alt="Error" className="warning" /> : <img src={Warning} alt="Error" className="warning" />}
             <h2>Cette page n&apos;existe pas!</h2>
         </motion.div>
     )
